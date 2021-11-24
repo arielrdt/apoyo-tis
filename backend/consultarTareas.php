fe0520bc4f8bda19c509ab06cd68de8e3ae57628
mitco874 <82779170+mitco874@users.noreply.github.com>
2021-11-24 13:01:55 -0400

    ver tarea crear grupos

--- backend/consultarTareas.php
+++ backend/consultarTareas.php
@@ -1,3 +1,50 @@
 <?php
-
 include("conexionBD.php");
+session_start(); 
+$cod_clase=$_SESSION['COD_CLASE'];
+
+function tareasDeClase($cod_clase,$conexionBD){
+
+$htmlTareas='';
+
+$consultaSQL="SELECT * FROM TAREA WHERE COD_CLASE='$cod_clase'";
+$ejecucionConsulta=mysqli_query($conexionBD,$consultaSQL);
+while($filaTabla=mysqli_fetch_array($ejecucionConsulta)){
+if($filaTabla['TIPO_TAREA']=="Formacion Grupos"){
+    $htmlTareas.="
+    <div class='section-card-tarea'>
+         <p class='card-title'>Formar grupos</p>
+            <div class='card-information'>
+            <p>Titulo:<span>".$filaTabla['TITULO_TAREA']."</span></p>
+            <p>Descripción:<span>".$filaTabla['DESCRIPCION_TAREA']."</span></p>
+            <p>Fecha de entrega:<span>".$filaTabla['FECHA_MAXIMA']."</span></p>
+            <p>hasta las :<span>".$filaTabla['HORA_MAXIMA']."</span></p>  
+            </div>
+        <a href='./crearGrupoEmpresa.html' class='boton-crear-grupos'>Crear grupo</a>
+    </div>
+    ";}
+
+else{
+    $htmlTareas.="
+    <div class='section-card-tarea'>
+         <p class='card-title'>Tarea</p>
+            <div class='card-information'>
+            <p>Titulo:<span>".$filaTabla['TITULO_TAREA']."</span></p>
+            <p>Descripción:<span>".$filaTabla['DESCRIPCION_TAREA']."</span></p>
+            <p>Fecha de entrega:<span>".$filaTabla['FECHA_MAXIMA']."</span></p>
+            <p>hasta las :<span>".$filaTabla['HORA_MAXIMA']."</span></p>  
+            </div>
+        <button type='submit' id='boton-ver-tarea'>Ver Tarea</button>
+    </div>
+    ";}
+
+
+}
+echo json_encode($htmlTareas);
+}
+
+tareasDeClase($cod_clase,$conexionBD);
+
+
+
+?>
