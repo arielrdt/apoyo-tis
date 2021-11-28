e1aa4dd9c5ff6b1a9f9ddf8cc949d2a820042b74
mitco874 <82779170+mitco874@users.noreply.github.com>
2021-11-28 15:36:57 -0400

    correcciones ortógraficas

--- backend/consultaPerfil.php
+++ backend/consultaPerfil.php
@@ -1,5 +1,10 @@
 <?php
 include("conexionBD.php");
-session_start(); 
-echo json_encode($_SESSION['ROL_CURSO'].'-'.$_SESSION['COD_CLASE']);
+session_start();
+$htmlPerfil='<h5>Nombre:'.$_SESSION['NOMBRE'].' '.$_SESSION['APELLIDO_PATERNO'].' '.$_SESSION['APELLIDO_MATERNO'].'('.$_SESSION['ROL_CURSO'].')</h5>
+<br>
+<h5>Clase:'.$_SESSION['COD_CLASE'].'</h5>
+'
+;
+echo json_encode($htmlPerfil);
 ?>
