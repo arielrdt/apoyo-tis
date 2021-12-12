71f6839f558a8486623da46d26b929bf640e3847
Ariel <arieldelgadillo444@gmail.com>
2021-12-12 11:58:54 -0400

    ver estudiantes por las grupo empresas terminado

--- backend/consultarGrupoEmpresas.php
+++ backend/consultarGrupoEmpresas.php
@@ -3,16 +3,43 @@ include("conexionBD.php");
 
 $query="SELECT * FROM grupo_empresa";
 $result=mysqli_query($conexionBD, $query);
-
+// $salidaEst='';
 $salida='';
 while ($filaGrupo=mysqli_fetch_array($result)) {
 
     $queryEst='SELECT * FROM estudiante WHERE NOMBRE_CORTO="'.$filaGrupo['NOMBRE_CORTO'].'" ';
     $resultEst=mysqli_query($conexionBD, $queryEst);
-    $filaEst=mysqli_fetch_array($resultEst);
+    // $filaEst=mysqli_fetch_array($resultEst);
+
+    $salidaEst='';
+    while ($filaEst=mysqli_fetch_array($resultEst)) {
+        if(isset($filaEst['NOMBRE_CORTO'])){
+            $salidaEst.='
+                <li class="student-content">
+                    <div class="student-content-photo-and-name">
+                        <img class="student-content-img" src="https://i.postimg.cc/D0qqChkv/linux-avatar.jpg" alt="">
+                        <p class="student-content-name">'.$filaEst['NOMBRE'].' '.$filaEst['APELLIDO_PATERNO'].' '.$filaEst['APELLIDO_MATERNO'].'</p>
+                    </div>
+                    <p class="student-content-rol">'.$filaEst['ROL'].'</p>
+                </li> 
+            ';
+        } else {
+            $salidaEst.='
+                <li class="student-content">
+                    <p class="student-content-rol">No se registraron estudiantes en esta grupo empresa</p>
+                </li>
+            ';
+        }
+    }
+    if ($salidaEst == '') {
+        $salidaEst='
+            <li class="student-content">
+                <p class="student-content-rol">No se registraron estudiantes en esta grupo empresa</p>
+            </li>
+        ';
+    }
 
     $salida.='
-
         <div class="main-clase-card">
             <div class="main-card-title">
                 <img class="main-card-image" src="https://i.postimg.cc/D0qqChkv/linux-avatar.jpg" alt="">
@@ -21,36 +48,16 @@ while ($filaGrupo=mysqli_fetch_array($result)) {
             <div class="main-card-list activo">
                 <h3>Integrantes</h3>
                 <ul id="main-section-list" class="main-section-list">
-    ';
-    if(isset($filaEst['NOMBRE_CORTO'])){
-                $salida.='
-                    <li class="student-content">
-                        <div class="student-content-photo-and-name">
-                            <img class="student-content-img" src="https://i.postimg.cc/D0qqChkv/linux-avatar.jpg" alt="">
-                            <p class="student-content-name">'.$filaEst['NOMBRE'].' '.$filaEst['APELLIDO_PATERNO'].' '.$filaEst['APELLIDO_MATERNO'].'</p>
-                        </div>
-                        <p class="student-content-rol">'.$filaEst['ROL'].'</p>
-                    </li>
+                    '.$salidaEst.'
                 </ul>
             </div>
         </div>
-
     ';
-    } else {
-                $salida.='
-                    <li class="student-content">
-                        <p class="student-content-rol">No se registraron estudiantes en esta grupo empresa</p>
-                    </li>
-                </ul>
-            </div>
-        </div> 
-    ';
-    }
 
 }
 
 echo json_encode('   
-    <h2 class="main-section-title-group">Flores Villarroel Corina</h2>
+    <h2 class="main-section-title-group"></h2>
     <div class="main-section-group">
         '.$salida.'
     </div>
